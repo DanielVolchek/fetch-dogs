b2b309f4ec7142699f10eee9e9a05a696f7e5eab
Daniel Volchek <danielvolchek123@gmail.com>
2025-02-05 14:01:23 -0800

    writing dog service

--- src/app/(home)/components/login.tsx
+++ src/app/(home)/components/login.tsx
@@ -5,13 +5,11 @@ import { ChangeEventHandler, useState } from "react";
 import { FetchSDKClient } from "@/lib/FetchSDK/client";
 
 export const Login = () => {
-  const fetchSDK = FetchSDKClient;
-
   const [username, setUsername] = useState("");
   const [password, setPasword] = useState("");
 
   const onSubmit = () => {
-    fetchSDK.login(username, password);
+    FetchSDKClient.login(username, password);
   };
 
   return (
--- src/lib/FetchSDK/index.ts
+++ src/lib/FetchSDK/index.ts
@@ -1,4 +1,6 @@
+import { internalFetch } from "../fetch";
 import { Maybe } from "../types";
+import { FetchSDKClient } from "./client";
 
 type FetchType<T> =
   | {
@@ -28,7 +30,10 @@ export class FetchSDK {
     options?: RequestInit,
   ): Promise<FetchType<T>> {
     try {
-      const res = await fetch(`${this.base_url}/${path}`, options);
+      // TODO default options object and merge
+      const _options: RequestInit = { credentials: "include", ...options };
+
+      const res = await fetch(`${this.base_url}/${path}`, _options);
 
       if (!res.ok) {
         throw new Error(`HTTP error! status: ${res.status}`);
@@ -40,9 +45,9 @@ export class FetchSDK {
       }
 
       const data = (await res.json()) as T;
-      return data;
+      return { result: data, error: null };
     } catch (err) {
-      return {};
+      return { result: null, error: { status: 400, message: `${err}` } };
     }
   }
 
@@ -53,5 +58,58 @@ export class FetchSDK {
     return "https://frontend-take-home-service.fetch.com";
   }
 
-  login(username: string, password: string) {}
+  async login(name: string, email: string) {
+    return await this.internalFetch("/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  async logout() {
+    return await this.internalFetch("/auth/logout", {
+      method: "POST",
+    });
+  }
+
+  // TODO create this as a service
+  // Move the internal fetch implementation to a service and inject it into all other services
+  DogService = {
+    getDogBreeds: async () => {
+      return await this.internalFetch<string[]>("/dogs/breeds");
+    },
+    getDogs: async (options: Partial<GetDogsOptions>) => {
+      const defaults: typeof options = { size: 25, sort: "breed:asc" };
+      const _options = { ...defaults, options };
+
+      return await this.internalFetch();
+    },
+  };
+
+  LocationService = {
+    async getLocationService() {},
+  };
+}
+
+type QueryFilter = "breed" | "name" | "age";
+type SortFilter<T extends string> = `${T}:${"asc" | "desc"}`;
+
+type GetDogsOptions = {
+  breeds: string[];
+  zipCodes: number[];
+  ageMin: number;
+  ageMax: number;
+  size: number;
+  from: number;
+  sort: SortFilter<QueryFilter>;
+};
+
+// apis
+// /dogs/breeds
+// /dogs/search
+// /dogs
+class DogClient {
+  constructor() {}
 }
